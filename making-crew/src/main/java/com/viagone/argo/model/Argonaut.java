c8a9370fd5f92741dec6c0e836a6ba318da7f245
n.morel <nicolas.morel@maformationenligne.bzh>
2021-02-15 17:33:03 +0100

    add a basic Model

---
+++ making-crew/src/main/java/com/viagone/argo/model/Argonaut.java
@@ -0,0 +1,60 @@
+package com.viagone.argo.model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Argonaut {
+    
+    private Long id;
+    private String fullname;
+
+    public Argonaut(Long id, String fullname) {
+        this.id = id;
+        this.fullname = fullname;        
+    }
+
+    @Override
+    public String toString() {
+        return "Argonaut [fullname=" + fullname + ", id=" + id + "]";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    /**
+     * 
+     * @param fullname
+     * @throws Exception if the parameter doesn't matchs the valid pattern (only letters and dashes)
+     */
+    public void setFullname(String fullname) throws Exception{
+        String fullnameRegex = "^[a-zA-Z-]+$";
+        Pattern p = Pattern.compile(fullnameRegex);
+        Matcher m = p.matcher(fullname);
+
+        if (m.matches() == false) {
+            throw new Exception("That's not a valid Argonaut name!");
+        }
+        if (fullname.isEmpty()) {
+            throw new Exception("Ghosts are not allowed on board! Please provide a name");
+        }
+
+        else {
+            this.fullname = fullname;
+        }
+        
+    }
+
+
+
+    
+    
+}
